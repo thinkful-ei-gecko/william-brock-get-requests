ed2b68ddb599464f0d68a7f1dff8230e291cf067
William Bae <wbae912@gmail.com>
2019-08-22 13:15:56 -0700

    dog breed app complete

--- dog-breed/index.js
+++ dog-breed/index.js
@@ -1,5 +1,6 @@
 'use strict';
 
+
 function getDogBreed(userInput) {
   fetch(`https://dog.ceo/api/breed/${userInput}/images/random`)
     .then(response => response.json())
@@ -8,18 +9,21 @@ function getDogBreed(userInput) {
       console.log(responseJson);
     })
     .catch(error => alert('Server Error Cant access'));
-
 }
 
 function displayResults(responseJson) {
   console.log(responseJson);
   //replace the existing image with the new one
-  $('.results-img').replaceWith(
-    `<img src="${responseJson.message}" class="results-img">`
-  );
+  if (responseJson.code === 404) {
+    alert('This is not a dog breed');
+  } else {
+    $('.results-img').replaceWith(
+      `<img src="${responseJson.message}" class="results-img">`);
+  }
   //display the results section
   $('.results').removeClass('hidden');
 }
+
 
 function watchForm() {
   $('form').submit(event => {
@@ -29,19 +33,17 @@ function watchForm() {
   });
 }
 
-function doesAThing() {
-  const userInput = $('#search-breed').val();
-  if (responseJson.code === 404) {
-    alert('This is not a dog breed');      
-  } else { 
-    getDogBreed(userInput);
-  }
-}
-
-
-
 
 $(function() {
   console.log('App loaded! Waiting for submit!');
   watchForm();
 });
+
+
+
+//console.log(responseJson);
+// If breed exists, we get an object(responseJson) that looks like this:
+// {message: "https://images.dog.ceo/breeds/hound-afghan/n02088094_1259.jpg" status: "success"__proto__: Object"}
+
+// If breed does NOT exist, we get an object(responseJson) that looks like this:
+// {status: "error", message: "Breed not found (master breed does not exist)", code: 404}
