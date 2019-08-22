5cd838ad1f5b9fd7abc66cae8ce62ab98e34e90d
William Bae <wbae912@gmail.com>
2019-08-22 11:30:42 -0700

    multiple dogs printing

--- index.js
+++ index.js
@@ -1,26 +1,57 @@
 'use strict';
 
 function getDogImage(number) {
-  fetch('https://dog.ceo/api/breeds/image/random')
+  fetch(`https://dog.ceo/api/breeds/image/random/${number}`)
     .then(response => response.json())
-    // .then(responseJson => {
-    //   console.log(responseJson)
-    // })
-    .then(responseJson => displayResults(responseJson))
-    .catch(error => console.log(error));
+    .then(responseJson => {
+      displayResults(responseJson);
+      console.log(responseJson);
+    })
+    .catch(error => alert('Something went wrong. Try again later.'));
 }
 
 
 function displayResults(responseJson) {
   // $('img').replaceWith(`<img src="${responseJson.message}" class="dog-picture">`);
-  $('.results').append(`<img src="${responseJson.message}" class="dog-picture">`);
+  $('.results').empty();
+
+  responseJson.message.forEach(img => $('.results').append(`<img src="${img}" class="dog-picture">`));
 }
 
-function displayXTimes(number) {
-  for (let i = 0; i <= number; i++) {
-    getDogImage();
-  }
+
+
+function handleSubmitButton() {
+  $('.dog-submit').on('submit', function(event) {
+    event.preventDefault();
+    console.log('hello world');
+    const userInput = $('#select-number').val(); 
+    getDogImage(userInput);
+  });
 }
+
+
+function main() {
+  handleSubmitButton();
+}
+
+
+$(main);
+
+
+
+
+//Why did't this work?
+// for (let i = 0; i < number; i++) {
+//   ('.results').append(`<img src="${img}" class="dog-picture">`);
+// }
+
+//IGNORE:
+
+// function displayXTimes(number) {
+//   for (let i = 0; i <= number; i++) {
+//     getDogImage();
+//   }
+// }
 
 // function generateImageHtml(responseJson) {
 //   return `
@@ -38,27 +69,3 @@ function displayXTimes(number) {
 //   for (let i = 1; i <= number; i++) {
 //     renderHtml(responseJson);
 //   }
-
-
-
-
-function handleSubmitButton() {
-  $('.dog-submit').on('submit', function(event) {
-    event.preventDefault();
-    console.log('hello world');
-    const userInput = $('#select-number').val(); //convert the string to number parseInt()
-    //function that takes in a number parameter and spits out X amount of generateImageHtml...ARGUMENT will be const userInput...
-    // renderHtml( , userInput)
-    // console.log(userInput);
-    displayXTimes(userInput);
-    getDogImage(userInput);
-  });
-}
-
-
-function main() {
-  handleSubmitButton();
-}
-
-
-$(main);
